1b06bed084fe360a1b6ffde460c8ad0c637e69cc
Grant R. Vousden-Dishington <GrantRVD@gmail.com>
2015-07-28 16:57:19 -0700

    Added basic 'feed' functionality.

--- interaction.js
+++ interaction.js
@@ -1,9 +1,17 @@
 var cat_image = document.getElementById('cat');
+var feed_button = document.getElementById('feed');
+
 cat_image.addEventListener("click", function() {
 	meow();
 	sleep();
 });
 
+feed_button.addEventListener("click", feed)
+
 function meow() {
 	alert("meow!");
 };
+
+function feed() {
+	alert("This offering pleases me, hooman.");
+}
