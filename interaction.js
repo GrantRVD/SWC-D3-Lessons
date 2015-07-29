de1575c63875a308f66fa1483395c966b465e469
Grant R. Vousden-Dishington <GrantRVD@gmail.com>
2015-07-29 14:24:58 -0700

    Changed button functions to increase or decrease cat image size.

--- interaction.js
+++ interaction.js
@@ -1,17 +1,23 @@
 var cat_image = document.getElementById('cat');
 var feed_button = document.getElementById('feed');
+var workout_button = document.getElementById('workout');
 
 cat_image.addEventListener("click", function() {
 	meow();
 	sleep();
 });
 
-feed_button.addEventListener("click", feed)
+feed_button.addEventListener("click", feed);
+workout_button.addEventListener("click", workout);
 
 function meow() {
 	alert("meow!");
 };
 
 function feed() {
-	alert("This offering pleases me, hooman.");
-}
+	cat_image.style.width = (cat_image.offsetWidth + 30) + 'px';
+};
+
+function workout() {
+	cat_image.style.width = (cat_image.offsetWidth - 30) + 'px';
+};
