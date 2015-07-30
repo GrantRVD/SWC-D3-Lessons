3dd25479dfc064baead187bcbd4838325467194b
Grant R. Vousden-Dishington <GrantRVD@gmail.com>
2015-07-30 15:25:07 -0700

    Added cat_object, added attributes, and playing with basic JSON notation.

--- interaction.js
+++ interaction.js
@@ -1,3 +1,4 @@
+// Stuff for lesson 05
 var cat_image = document.getElementById('cat');
 var feed_button = document.getElementById('feed');
 var workout_button = document.getElementById('workout');
@@ -15,9 +16,21 @@ function meow() {
 };
 
 function feed() {
-	cat_image.style.width = (cat_image.offsetWidth + 30) + 'px';
+	cat_image.style.width = (cat_image.offsetWidth + 30.0) + 'px';
 };
 
 function workout() {
-	cat_image.style.width = (cat_image.offsetWidth - 30) + 'px';
+	cat_image.style.width = (cat_image.offsetWidth - 30.0) + 'px';
 };
+
+// Stuff for Lesson 06
+var cat_object = {
+	weight: 5,
+	past_weight_values: [4.5, 5.1, 4.9],
+	name: "Princess Caroline"
+};
+
+cat_object.height = 10
+
+var cat_list = [cat_object];
+cat_list.push({weight: 5, past_weight_values: [5.9, 5.3, 6.1], name: "Snowball"});
